a4a9bdb36c47662eb746c6c3555a837e0b551f97
TIMOTHY D FIRMAN <141569413+chimohe@users.noreply.github.com>
2025-07-13 12:46:16 -0400

    Create config.js

---
+++ btc-watcher/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  watchAddress: "your-btc-receive-address",
+  confirmations: 3,
+  rpcUrl: "https://goerli.infura.io/v3/YOUR_INFURA_KEY",
+  privateKey: "0xYOUR_PRIVATE_KEY",
+  bridgeContract: "0xBRIDGE_CONTRACT_ADDRESS",
+  wrappedBTCContract: "0xWRAPPED_BTC_CONTRACT_ADDRESS"
+};

4506b58a87c347db40d1392173aa057f90190c8c
TIMOTHY D FIRMAN <141569413+chimohe@users.noreply.github.com>
2025-07-13 12:49:18 -0400

    Create mintFromWatcher.js

---
+++ scripts/mintFromWatcher.js
@@ -0,0 +1,16 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+  const bridge = await ethers.getContract("BTCBridge", signer);
+
+  const recipient = signer.address;
+  const amount = ethers.parseUnits("0.01", 8); // 0.01 BTC
+  const btcTxId = ethers.keccak256(ethers.toUtf8Bytes("dummy_tx_hash"));
+
+  const tx = await bridge.mintFromBTC(recipient, amount, btcTxId);
+  await tx.wait();
+  console.log("Minted wBTC from BTC tx:", btcTxId);
+}
+
+main().catch(console.error);

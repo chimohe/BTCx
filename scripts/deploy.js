910a6f2907fb2678dbb191c99fa2b75264674ee3
TIMOTHY D FIRMAN <141569413+chimohe@users.noreply.github.com>
2025-07-14 14:21:45 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,12 @@ async function main() {
   console.log("Deploying contracts with:", deployer.address);
 
   const WrappedBTC = await ethers.getContractFactory("WrappedBTC");
-  const wBTC = await WrappedBTC.deploy();
-  await wBTC.deployed();
+  const BTCx = await WrappedBTC.deploy();
+  await BTCx.deployed();
   console.log("WrappedBTC deployed to:", wBTC.address);
 
   const BTCBridge = await ethers.getContractFactory("BTCBridge");
-  const bridge = await BTCBridge.deploy(wBTC.address);
+  const bridge = await BTCBridge.deploy(BTCx.address);
   await bridge.deployed();
   console.log("BTCBridge deployed to:", bridge.address);
 }

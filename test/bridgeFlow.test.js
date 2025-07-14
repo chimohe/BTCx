7bdfe32618f5f48705a68681c5fc824ebf93c733
TIMOTHY D FIRMAN <141569413+chimohe@users.noreply.github.com>
2025-07-14 14:26:54 -0400

    Update bridgeFlow.test.js

--- test/bridgeFlow.test.js
+++ test/bridgeFlow.test.js
@@ -2,18 +2,18 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("BTCBridge", () => {
-  let wBTC, bridge, owner, user;
+  let BTCx, bridge, owner, user;
 
   beforeEach(async () => {
     [owner, user] = await ethers.getSigners();
     const WrappedBTC = await ethers.getContractFactory("WrappedBTC");
-    wBTC = await WrappedBTC.deploy();
-    await wBTC.deployed();
+    BTCx = await WrappedBTC.deploy();
+    await BTCx.deployed();
 
     const BTCBridge = await ethers.getContractFactory("BTCBridge");
     bridge = await BTCBridge.deploy(wBTC.address);
     await bridge.deployed();
-    await wBTC.transferOwnership(bridge.address);
+    await BTCx.transferOwnership(bridge.address);
   });
 
   it("mints wBTC from BTC tx", async () => {
@@ -25,8 +25,8 @@ describe("BTCBridge", () => {
   it("burns wBTC for BTC redemption", async () => {
     const btcTxId = ethers.keccak256(ethers.toUtf8Bytes("btc_tx_hash"));
     await bridge.mintFromBTC(user.address, 1000, btcTxId);
-    await wBTC.connect(user).approve(bridge.address, 1000);
+    await BTCx.connect(user).approve(bridge.address, 1000);
     await bridge.connect(user).burnForBTC(1000);
-    expect(await wBTC.balanceOf(user.address)).to.equal(0);
+    expect(await BTCx.balanceOf(user.address)).to.equal(0);
   });
 });

e7bd30347b29a054e039f23e022fe66d9873ef39
TIMOTHY D FIRMAN <141569413+chimohe@users.noreply.github.com>
2025-07-14 14:20:45 -0400

    Update burnToReleaseBTC.js

--- scripts/burnToReleaseBTC.js
+++ scripts/burnToReleaseBTC.js
@@ -7,7 +7,7 @@ async function main() {
   const amount = ethers.parseUnits("0.005", 8); // 0.005 BTC
   const tx = await bridge.burnForBTC(amount);
   await tx.wait();
-  console.log("wBTC burned to initiate BTC release");
+  console.log("BTCx burned to initiate BTC release");
 }
 
 main().catch(console.error);
